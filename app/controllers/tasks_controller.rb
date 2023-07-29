a51a46f889bf088216753a7c14bfda9b8f2c58f9
Fernando Almeida <nando.a.g.martines@gmail.com>
2023-07-29 16:56:43 -0300

    Wikinimous - My Tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Rota para listar todas as tarefas
+  get 'tasks', to: 'tasks#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Rota para exibir o formulário de criação de uma nova tarefa
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # Rota para criar uma nova tarefa quando o formulário é submetido
+  post 'tasks', to: 'tasks#create'
+
+  # Rota para exibir os detalhes de uma tarefa específica
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Rota para exibir o formulário de edição de uma tarefa existente
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # Rota para atualizar uma tarefa quando o formulário de edição é submetido
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Rota para excluir uma tarefa
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # Utilizando o helper 'resources' para definir todas as rotas padrão para as tarefas
+  resources :tasks
 end
